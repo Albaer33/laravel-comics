06a92d1db86ebb93c06c8b237c54a70a5bff5867
Albaer33 <alisi3396@gmail.com>
2022-02-09 17:23:19 +0100

    latest push

--- resources/views/homepage.blade.php
+++ resources/views/homepage.blade.php
@@ -5,5 +5,15 @@ homepage
 @endsection
 
 @section('main_content')
-ciao sono il main
+<section class="bg-black">
+    <div class="container">
+        <div class="comics">
+            @foreach ($comics_array as $comic)
+                <div class="single-comic">
+                    <img src="{{$comic['thumb']}}" alt="{{$comic['title']}}">
+                </div>
+            @endforeach
+        </div>
+    </div>
+</section>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('homepage');
+    $comics_array = config('comics');
+
+    return view('homepage', $comics);
 });
