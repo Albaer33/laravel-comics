d1299686d789c6ce78fd9727553c3e5bd2a2e160
Albaer33 <alisi3396@gmail.com>
2022-02-09 16:09:43 +0100

    added img in public folder

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,3 +1,22 @@
-<header>
-    ciao sono l'header
+<header class="container">
+    <div class="small-band">DC power visa</div>
+    <div class="header-content">
+        <div class="logo">
+            <img src="" alt="">
+        </div>
+        <div class="menu">
+            <ul>
+                <li><a href="">CHARACTERS</a></li>
+                <li><a href="">COMICS</a></li>
+                <li><a href="">MOVIES</a></li>
+                <li><a href="">TV</a></li>
+                <li><a href="">GAMES</a></li>
+                <li><a href="">COLLECTIBLES</a></li>
+                <li><a href="">VIDEOS</a></li>
+                <li><a href="">FANS</a></li>
+                <li><a href="">NEWS</a></li>
+                <li><a href="">SHOP</a></li>
+            </ul>
+        </div>
+    </div>
 </header>
